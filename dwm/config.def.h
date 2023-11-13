886a13cf79065610bb2e92afea371f4a2a42b565
Vic <victenbokum@gmail.com>
2023-11-13 13:20:40 +0100

    added thunderbird launch and changed keybindings

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -89,21 +89,25 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *roficmd[] = { "rofi", "-show", "drun", "-show-icons", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "alacritty", "-t", scratchpadname, NULL };
+/* Launching apps */
 static const char *browsercmd[]  = { "thorium-browser", NULL };
 static const char *filescmd[]  = { "thunar", NULL };
 static const char *lockcmd[]  = { "slock", NULL };
-static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "alacritty", "-t", scratchpadname, NULL };
+static const char *emailcmd[]  = { "thunderbird", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
 	{ MODKEY,                       XK_x,      spawn,          {.v = dmenucmd } },
     { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_w,      spawn,          {.v = browsercmd } },
-	{ MODKEY,                       XK_e,      spawn,          {.v = filescmd } },
+    { MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
+    /* Launching apps */
+	{ Mod4Mask,                     XK_b,      spawn,          {.v = browsercmd } },
+	{ Mod4Mask,                     XK_e,      spawn,          {.v = filescmd } },
 	{ Mod4Mask,                     XK_l,      spawn,          {.v = lockcmd } },
- 	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
+	{ Mod4Mask,                     XK_m,      spawn,          {.v = emailcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
