1a44b412da15cb5439b4b53f2a5bd5776e329855
xxheyhey <95343562+xxheyhey@users.noreply.github.com>
2024-03-20 21:11:43 +0100

    gimp not floating by default

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -41,7 +41,6 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "R_x11",    NULL,       NULL,	      0,            1,           -1 },
 };
 
