aa69c3071da7e666b6bcdc52b3c5c5491100459b
xxheyhey <95343562+xxheyhey@users.noreply.github.com>
2024-11-28 21:04:53 +0100

    mouse moving now 165fps

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -1410,7 +1410,7 @@ movemouse(const Arg *arg)
 			handler[ev.type](&ev);
 			break;
 		case MotionNotify:
-			if ((ev.xmotion.time - lasttime) <= (1000 / 60))
+			if ((ev.xmotion.time - lasttime) <= (1000 / 165))
 				continue;
 			lasttime = ev.xmotion.time;
 
@@ -1610,7 +1610,7 @@ resizemouse(const Arg *arg)
 			handler[ev.type](&ev);
 			break;
 		case MotionNotify:
-			if ((ev.xmotion.time - lasttime) <= (1000 / 60))
+			if ((ev.xmotion.time - lasttime) <= (1000 / 165))
 				continue;
 			lasttime = ev.xmotion.time;
 
